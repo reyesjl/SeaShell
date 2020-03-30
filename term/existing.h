502ebddd9eac2b2b5439fc096ef4af69de6c1d09
reyesjl <jreyes@mail.umw.edu>
2020-03-30 15:47:33 -0400

    more comments

--- term/existing.h
+++ term/existing.h
@@ -8,13 +8,13 @@ int sea_help(char **args);
 int sea_exit(char **args);
 
 // the commands with the corresponding function
-char *existing_str[] = {
-  "cd",
+char *existing_str[] = {                                          // existing names of system calls
+  "cd",                                                           // check these in sea_execute with strcmp()
   "help",
   "exit"
 };
 
-int (*existing_func[]) (char **) = {
+int (*existing_func[]) (char **) = {                              // corresponding functions.
   &sea_cd,
   &sea_help,
   &sea_exit
@@ -28,25 +28,25 @@ int sea_num_existing() {
 // existing change directory command
 int sea_cd(char **args)
 {
-  if (args[1] == NULL) {
+  if (args[1] == NULL) {                                            // cd arguments not enough
     fprintf(stderr, "sea: expected argument to \"cd\"\n");
   } else {
-    if (chdir(args[1]) != 0) {
-      perror("sea: with cd command");
+    if (chdir(args[1]) != 0) {                                      // call chdir()
+      perror("sea: with cd command");                               // handle error
     }
   }
-  return 1;
-}
+  return 1;                                                         // ran well.
+} 
 
 // help command
 int sea_help(char **args)
 {
   int i;
-  printf("SEA SHELL a.k.a C Shell\n");
+  printf("SEA SHELL a.k.a C Shell\n");                             // handle a help command
   printf("Its a semi-full functioning terminal!\n");
   printf("The following are built in:\n");
 
-  for (i = 0; i < sea_num_existing(); i++) {
+  for (i = 0; i < sea_num_existing(); i++) {                       // print out the existing commands.
     printf("  %s\n", existing_str[i]);
   }
 
@@ -54,7 +54,7 @@ int sea_help(char **args)
 }
 
 // exit command
-int sea_exit(char **args)
+int sea_exit(char **args)                                           // exit command
 {
   return 0;
 }
